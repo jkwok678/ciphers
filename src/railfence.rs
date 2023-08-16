2b6bf4c6ffe815e06eb2458c8983e716b09af32c
jkwok678 <jkwok678@gmail.com>
2023-08-16 21:46:34 +0100

    Fix railfence decryption finally

--- src/railfence.rs
+++ src/railfence.rs
@@ -9,10 +9,32 @@ pub fn railfence_cipher(text: &str, rails: usize, encrypt: bool) -> String {
         if rail == 0 || rail == rails - 1 {
             down = !down;
         }
-
         rail = if down { rail + 1 } else { rail - 1 };
     }
-    new_text = fence.iter().flatten().filter_map(|&char| char).collect();
+
+    if encrypt {
+        new_text = fence.iter().flatten().filter_map(|&char| char).collect();
+    } else {
+        let mut decrypted_index = 0;
+        for rail in 0..rails {
+            for col in 0..characters.len() {
+                if fence[rail][col].is_some() {
+                    fence[rail][col] = Some(characters[decrypted_index]);
+                    decrypted_index += 1;
+                }
+            }
+        }
+        rail = 0;
+        for index in 0..characters.len() {
+            if fence[rail][index].is_some() {
+                new_text.push(fence[rail][index].unwrap());
+            }
+            if rail == 0 || rail == rails - 1 {
+                down = !down;
+            }
+            rail = if down { rail + 1 } else { rail - 1 };
+        }
+    }
     return new_text;
 }
 
@@ -28,5 +50,15 @@ mod tests {
 
         let ciphertext = railfence_cipher(plain_text, num_rails, true);
         assert_eq!(ciphertext, expected_ciphertext);
+    }
+
+    #[test]
+    fn railfence_cipher_decrypt_simple() {
+        let ciphertext = "HOREL OLLWD";
+        let expected_plain_text = "HELLO WORLD";
+        let num_rails = 3;
+
+        let decoded_text = railfence_cipher(ciphertext, num_rails, false);
+        assert_eq!(decoded_text, expected_plain_text);
     }
 }
