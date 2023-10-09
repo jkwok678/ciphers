7371107dea57a5719066e6c6b5729eca6f37399f
jkwok678 <jkwok678@gmail.com>
2023-10-09 18:49:02 +0100

    Add decrypting column transposition

--- src/column_transposition.rs
+++ src/column_transposition.rs
@@ -1,4 +1,4 @@
-pub fn column_transposition_cipher(text: &str, key: &str) -> String {
+pub fn column_transposition_cipher(text: &str, key: &str, encrypt: bool) -> String {
     let mut new_text = String::new();
     let key_chars: Vec<_> = key.chars().collect();
     let key_len = key.len();
@@ -8,20 +8,46 @@ pub fn column_transposition_cipher(text: &str, key: &str) -> String {
         rows += 1;
     }
     let mut grid: Vec<Vec<Option<char>>> = vec![vec![None; rows]; columns];
-    for (i, c) in text.chars().enumerate() {
-        let col = i % columns;
-        let row = i / columns;
-        grid[col][row] = Some(c);
-    }
-    let mut sorted_columns: Vec<_> = (0..key_len).collect();
-    sorted_columns.sort_by_key(|&col| &key_chars[col]);
-    for col in sorted_columns {
+    if encrypt {
+        for (i, c) in text.chars().enumerate() {
+            let col = i % columns;
+            let row = i / columns;
+            grid[col][row] = Some(c);
+        }
+        let mut sorted_columns: Vec<_> = (0..key_len).collect();
+        sorted_columns.sort_by_key(|&col| &key_chars[col]);
+        for col in sorted_columns {
+            for row in 0..rows {
+                if let Some(c) = grid[col][row] {
+                    new_text.push(c);
+                } else {
+                    new_text.push('_')
+                }
+            }
+        }
+    } else {
+        let mut sorted_columns: Vec<_> = (0..key_len).collect();
+        sorted_columns.sort_by_key(|&col| &key_chars[col]);
+        let mut index = 0;
+        for col in sorted_columns {
+            for row in 0..rows {
+                if index < text.len() {
+                    grid[col][row] = Some(text.chars().nth(index).unwrap());
+                    index += 1;
+                }
+            }
+        }
         for row in 0..rows {
-            if let Some(c) = grid[col][row] {
-                new_text.push(c);
+            for col in 0..columns {
+                if let Some(c) = grid[col][row] {
+                    if c != '_' {
+                        new_text.push(c);
+                    }
+                }
             }
         }
     }
+
     new_text
 }
 
@@ -33,9 +59,9 @@ mod tests {
     fn encrypt_upper() {
         let plain_text = "HELLOWORLD";
         let key = "KEY";
-        let expected_ciphertext = "EORHLODLWL";
+        let expected_ciphertext = "EOR_HLODLWL_";
 
-        let ciphertext = column_transposition_cipher(plain_text, key);
+        let ciphertext = column_transposition_cipher(plain_text, key, true);
         assert_eq!(ciphertext, expected_ciphertext);
     }
 
@@ -43,9 +69,9 @@ mod tests {
     fn encrypt_lower() {
         let plain_text = "helloworld";
         let key = "KEY";
-        let expected_ciphertext = "eorhlodlwl";
+        let expected_ciphertext = "eor_hlodlwl_";
 
-        let ciphertext = column_transposition_cipher(plain_text, key);
+        let ciphertext = column_transposition_cipher(plain_text, key, true);
         assert_eq!(ciphertext, expected_ciphertext);
     }
 
@@ -53,9 +79,9 @@ mod tests {
     fn encrypt_with_space() {
         let plain_text = "HELLO WORLD";
         let key = "KEY";
-        let expected_ciphertext = "EOODHLWLL R";
+        let expected_ciphertext = "EOODHLWLL R_";
 
-        let ciphertext = column_transposition_cipher(plain_text, key);
+        let ciphertext = column_transposition_cipher(plain_text, key, true);
         assert_eq!(ciphertext, expected_ciphertext);
     }
 
@@ -63,9 +89,49 @@ mod tests {
     fn encrypt_with_puncuation() {
         let plain_text = "hello, world!";
         let key = "KEY";
-        let expected_ciphertext = "eowlhl r!l,od";
+        let expected_ciphertext = "eowl_hl r!l,od_";
 
-        let ciphertext = column_transposition_cipher(plain_text, key);
+        let ciphertext = column_transposition_cipher(plain_text, key, true);
         assert_eq!(ciphertext, expected_ciphertext);
+    }
+
+    #[test]
+    fn decrypt_upper() {
+        let cipher_text = "EOR_HLODLWL_";
+        let key = "KEY";
+        let expected_plain_text = "HELLOWORLD";
+
+        let plain_text = column_transposition_cipher(cipher_text, key, false);
+        assert_eq!(plain_text, expected_plain_text);
+    }
+
+    #[test]
+    fn decrypt_lower() {
+        let cipher_text = "eor_hlodlwl_";
+        let key = "KEY";
+        let expected_plain_text = "helloworld";
+
+        let plain_text = column_transposition_cipher(cipher_text, key, false);
+        assert_eq!(plain_text, expected_plain_text);
+    }
+
+    #[test]
+    fn decrypt_with_space() {
+        let cipher_text = "EOODHLWLL R_";
+        let key = "KEY";
+        let expected_plain_text = "HELLO WORLD";
+
+        let plain_text = column_transposition_cipher(cipher_text, key, false);
+        assert_eq!(plain_text, expected_plain_text);
+    }
+
+    #[test]
+    fn decrypt_with_puncuation() {
+        let cipher_text = "eowl_hl r!l,od_";
+        let key = "KEY";
+        let expected_plain_text = "hello, world!";
+
+        let plain_text = column_transposition_cipher(cipher_text, key, false);
+        assert_eq!(plain_text, expected_plain_text);
     }
 }
